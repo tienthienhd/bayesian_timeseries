35e2fc6e6d69ba2c2aa5d60dd3cb540a34f0e7cd
Tien Thien <tienthienhd@gmail.com>
2019-05-06 09:07:09 +0700

    edit code model

--- model.py
+++ model.py
@@ -5,43 +5,34 @@ import numpy as np
 import json
 import utils
 
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 
 class Model(object):
-    def __init__(self, model_dir):
+    def __init__(self, model_dir, params=None):
         self.model_dir = model_dir
-
-        self.layer_sizes_ed = []
-        self.layer_sizes_f = []
-        self.activation = tf.nn.tanh
-        self.keep_probs = 0.95
-        self.dropout = 0.05
-        self.sle = 1
-        self.sld = 1
-        self.n_dim = 1
-        self.optimizer = tf.train.AdamOptimizer
-        self.learning_rate = 0.1
-        self.cell_type = tf.nn.rnn_cell.LSTMCell
-        self.patience = 1
-
-
-        self.xe = None
-        self.xd = None
-        self.yd = None
-        self.yf = None
-        self.pred_d = None
-        self.pred_f = None
-        self.loss_ed = None
-        self.loss_f = None
-        self.mae_ed = None
-        self.mae_f = None
-        self.rmse_ed = None
-        self.rmse_f = None
-        self.train_op_ed = None
-        self.train_op_f = None
-
         tf.reset_default_graph()
         self.sess = tf.Session()
+
+        if params is not None:
+            self.params = params
+            self.sle = int(params['sliding_encoder'])
+            self.sld = int(params['sliding_decoder'])
+            self.layer_sizes_ed = params['layer_sizes_ed']
+            self.layer_sizes_f = params['layer_sizes_f']
+            self.n_dim = int(params['n_dim'])
+            self.keep_probs = params['keep_probs']
+            self.dropout = params['dropout']
+            self.patience = params['patience']
+            self.learning_rate = params['learning_rate']
+
+            self.activation = utils.transform_activation(params['activation'])
+            self.optimizer = utils.transform_optimizer(params['optimizer'])
+            self.cell_type = utils.transform_cell_type(params['cell_type'])
+
+            self.build_model()
+        else:
+            self.restore()
 
     def build_model(self, params):
         self._parse_params(params)
@@ -85,6 +76,10 @@ class Model(object):
             self.train_op_ed = self.optimizer(self.learning_rate).minimize(self.loss_ed)
             self.train_op_f = self.optimizer(self.learning_rate).minimize(self.loss_f)
 
+        # initial variable
+        self.sess.run(tf.global_variables_initializer())
+
+    def save(self):
         # add to collection
         tf.add_to_collection('params', self.xe)
         tf.add_to_collection('params', self.xd)
@@ -101,10 +96,6 @@ class Model(object):
         tf.add_to_collection('params', self.train_op_ed)
         tf.add_to_collection('params', self.train_op_f)
 
-        # initial variable
-        self.sess.run(tf.global_variables_initializer())
-
-    def save(self):
         saver = tf.train.Saver()
         saver.save(self.sess, self.model_dir + '/model')
 
@@ -131,33 +122,7 @@ class Model(object):
         self.train_op_ed = params[12]
         self.train_op_f = params[13]
 
-    def train(self, x, y, validation_split=0.2, batch_size=32, epochs=1, verbose=1, model='ed'):
-
-        if model == 'ed':
-            return self._train_ed(x, y, validation_split, batch_size, epochs, verbose)
-        elif model == 'f':
-            return self._train_f(x, y, validation_split, batch_size, epochs, verbose)
-
-    def eval(self, x, y, model='ed'):
-        if model == 'ed':
-            return self.sess.run([self.loss_ed, self.mae_ed], {
-                self.xe: x[0],
-                self.xd: x[1],
-                self.yd: y
-            })
-        elif model == 'f':
-            return self.sess.run([self.loss_f, self.mae_f], {
-                self.xe: x,
-                self.yf: y
-            })
-
-    def predict(self, x, model='ed'):
-        if model == 'ed':
-            return self.sess.run(self.pred_d, {self.xe: x[0], self.xd: x[1]})
-        elif model == 'f':
-            return self.sess.run(self.pred_f, {self.xe: x})
-
-    def _parse_params(self, params):
+        params = json.load(open(self.model_dir + '/hyper_params.json', 'r'))
         self.params = params
         self.sle = int(params['sliding_encoder'])
         self.sld = int(params['sliding_decoder'])
@@ -173,40 +138,7 @@ class Model(object):
         self.optimizer = utils.transform_optimizer(params['optimizer'])
         self.cell_type = utils.transform_cell_type(params['cell_type'])
 
-
-    def _create_block_rnn(self, inputs, state=None):
-        cells = []
-        for i, units in enumerate(self.layer_sizes_ed):
-            # create cell
-            cell = self.cell_type(num_units=units, activation=self.activation, name="layer_" + str(i))
-
-            # Wrap cell with dropout
-            cell = tf.nn.rnn_cell.DropoutWrapper(cell, input_keep_prob=self.keep_probs,
-                                                 output_keep_prob=self.keep_probs,
-                                                 state_keep_prob=self.keep_probs,
-                                                 variational_recurrent=True,
-                                                 input_size=self.n_dim if i == 0 else self.layer_sizes_ed[i - 1],
-                                                 dtype=tf.float32)
-            cells.append(cell)
-        # Multi cell layer
-        cells = tf.nn.rnn_cell.MultiRNNCell(cells)
-        if state is None:
-            output, state = tf.nn.dynamic_rnn(cells, inputs=inputs, dtype=tf.float32)
-        else:
-            output, state = tf.nn.dynamic_rnn(cells, inputs=inputs, initial_state=state, dtype=tf.float32)
-
-        return output, state
-
-    def _create_block_dense(self, inputs):
-        net = inputs
-        for i, units in enumerate(self.layer_sizes_f):
-            net = tf.layers.dense(net, units=units, activation=self.activation, name='layer_%d' % i)
-            net = tf.layers.dropout(net, rate=self.dropout)
-        net = tf.layers.dense(net, units=1, activation=self.activation)
-        output = tf.reshape(net, (-1, self.sle))
-        return output
-
-    def _train_ed(self, x, y, validation_split, batch_size, epochs, verbose):
+    def train_ed(self, x, y, validation_split=0.2, batch_size=32, epochs=1, verbose=1):
         n_train = int(len(y) * (1 - validation_split))
         xe_train = x[0][:n_train]
         xd_train = x[1][:n_train]
@@ -245,35 +177,23 @@ class Model(object):
                 }
                 output_feed = [self.loss_ed, self.mae_ed, self.train_op_ed]
 
-                try:
-                    l, m, _ = self.sess.run(output_feed, input_feed)
+                l, m, _ = self.sess.run(output_feed, input_feed)
 
-                    loss += l
-                    mae += m
-                except ValueError as e:
-                    print('==========> Exception: {}'.format(str(self.params)))
-                    print('________{}'.format(xd.shape))
-                    print(e)
-                    break
+                loss += l
+                mae += m
             loss /= n_batches
             mae /= n_batches
             history['loss'].append(loss)
             history['mae'].append(mae)
 
-            try:
-                val_loss, val_mae = self.sess.run([self.loss_ed, self.mae_ed],
-                                                  {
-                                                      self.xe: xe_val,
-                                                      self.xd: xd_val,
-                                                      self.yd: yd_val
-                                                  })
-                history['val_loss'].append(val_loss)
-                history['val_mae'].append(val_mae)
-            except ValueError as e:
-                print('==========> Exception: {}'.format(str(self.params)))
-                print('________{}'.format(xd_val.shape))
-                print(e)
-                break
+            val_loss, val_mae = self.sess.run([self.loss_ed, self.mae_ed],
+                                              {
+                                                  self.xe: xe_val,
+                                                  self.xd: xd_val,
+                                                  self.yd: yd_val
+                                              })
+            history['val_loss'].append(val_loss)
+            history['val_mae'].append(val_mae)
 
             epoch_time = time.time() - start_epoch_time
             if verbose > 0:
@@ -288,7 +208,7 @@ class Model(object):
                 break
         return history
 
-    def _train_f(self, x, y, validation_split, batch_size, epochs, verbose):
+    def train_f(self, x, y, validation_split=0.2, batch_size=0.2, epochs=1, verbose=1):
         n_train = int(len(y) * (1 - validation_split))
         xe_train = x[:n_train]
         yf_train = y[:n_train]
@@ -350,3 +270,54 @@ class Model(object):
             if np.isnan(loss):
                 break
         return history
+
+    def eval_ed(self, x, y):
+        return self.sess.run([self.loss_ed, self.mae_ed], {
+            self.xe: x[0],
+            self.xd: x[1],
+            self.yd: y
+        })
+
+    def eval_f(self, x, y):
+        return self.sess.run([self.loss_f, self.mae_f], {
+            self.xe: x,
+            self.yf: y
+        })
+
+    def predict_ed(self, x):
+        return self.sess.run(self.pred_d, {self.xe: x[0], self.xd: x[1]})
+
+    def predict_f(self, x):
+        return self.sess.run(self.pred_f, {self.xe: x})
+
+    def _create_block_rnn(self, inputs, state=None):
+        cells = []
+        for i, units in enumerate(self.layer_sizes_ed):
+            # create cell
+            cell = self.cell_type(num_units=units, activation=self.activation, name="layer_" + str(i))
+
+            # Wrap cell with dropout
+            cell = tf.nn.rnn_cell.DropoutWrapper(cell, input_keep_prob=self.keep_probs,
+                                                 output_keep_prob=self.keep_probs,
+                                                 state_keep_prob=self.keep_probs,
+                                                 variational_recurrent=True,
+                                                 input_size=self.n_dim if i == 0 else self.layer_sizes_ed[i - 1],
+                                                 dtype=tf.float32)
+            cells.append(cell)
+        # Multi cell layer
+        cells = tf.nn.rnn_cell.MultiRNNCell(cells)
+        if state is None:
+            output, state = tf.nn.dynamic_rnn(cells, inputs=inputs, dtype=tf.float32)
+        else:
+            output, state = tf.nn.dynamic_rnn(cells, inputs=inputs, initial_state=state, dtype=tf.float32)
+
+        return output, state
+
+    def _create_block_dense(self, inputs):
+        net = inputs
+        for i, units in enumerate(self.layer_sizes_f):
+            net = tf.layers.dense(net, units=units, activation=self.activation, name='layer_%d' % i)
+            net = tf.layers.dropout(net, rate=self.dropout)
+        net = tf.layers.dense(net, units=1, activation=self.activation)
+        output = tf.reshape(net, (-1, self.sle))
+        return output
